1345b7c73a53ca7f5289ecba7f1351becb8de7e3
AGU_PRO <arguerin@student.42.fr>
2022-05-02 01:58:25 +0200

    db_update
    db_update

---
+++ gen_db/flask_db_public_images.sql
@@ -0,0 +1,6 @@
+insert into public.images (id, title, path, profil_id, date_added)
+values  (1, 'Sammy Image', 'https://www.arthurguerin.com/assets/www.png', 1, '2022-03-28'),
+        (3, '9fc8139f0e9b241005a2bc7ff5e7ee7f502d4c093a2500205e0ce61ea57d4eea/Logo_noir_fond_transparent.png', '9fc8139f0e9b241005a2bc7ff5e7ee7f502d4c093a2500205e0ce61ea57d4eea/Logo_noir_fond_transparent.png', 27, '2022-04-24'),
+        (4, '9fc8139f0e9b241005a2bc7ff5e7ee7f502d4c093a2500205e0ce61ea57d4eea/Voodoo-02.jpg', '9fc8139f0e9b241005a2bc7ff5e7ee7f502d4c093a2500205e0ce61ea57d4eea/Voodoo-02.jpg', 27, '2022-04-24'),
+        (6, '9fc8139f0e9b241005a2bc7ff5e7ee7f502d4c093a2500205e0ce61ea57d4eea/Voodoo-02.jpg', '9fc8139f0e9b241005a2bc7ff5e7ee7f502d4c093a2500205e0ce61ea57d4eea/Voodoo-02.jpg', 27, '2022-04-24'),
+        (8, '87aa3ddef63abf5f088752b8d40d5fff3aa47e5176fde1bc7e0eada3d787c424/Voodoo-02.jpg', '87aa3ddef63abf5f088752b8d40d5fff3aa47e5176fde1bc7e0eada3d787c424/Voodoo-02.jpg', 28, '2022-04-25');
---
+++ gen_db/flask_db_public_profil.sql
@@ -0,0 +1,4 @@
+insert into public.profil (id, user_id, genre_id, orientation_id, location_id, age, bio, image_profil, score, is_online, last_log)
+values  (4, 28, 2, 1, 4, '2007-04-01', 'Je suis l`ex test', 0, null, false, '2022-04-25'),
+        (1, 3, 1, 2, 0, '1988-04-08', '"hey . hey hey"', 0, 4, false, '2022-04-17'),
+        (3, 27, 1, 1, 3, '2000-04-11', 'c`est un nouveau test', 0, 0, false, '2022-04-19');
